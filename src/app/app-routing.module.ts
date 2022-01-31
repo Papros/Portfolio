79535d621129fb8c3b3fd88bf279589f3dd35d84
Piotr <piotrpa0@op.pl>
2022-01-31 22:25:52 +0100

    Fixing gh-pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { NoPreloading, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{
   path: '',
   pathMatch: 'full',
-  redirectTo: '/${home}',
+  redirectTo: '/menu',
 }];
 
 @NgModule({
