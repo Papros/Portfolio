07ce12b64f1b97824d6e6acb04d8709fb6aed018
Piotr <piotrpa0@op.pl>
2022-02-07 16:13:54 +0100

    Moved tile-menu-item styling to child component

--- src/app/modules/shared/tile-menu/components/tile-menu-item/tile-menu-item.component.ts
+++ src/app/modules/shared/tile-menu/components/tile-menu-item/tile-menu-item.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit } from '@angular/core';
+import { SafeStyle } from '@angular/platform-browser';
 import { ITileConfig } from '../../interfaces';
 
 @Component({
@@ -9,9 +10,43 @@ import { ITileConfig } from '../../interfaces';
 })
 export class TileMenuItemComponent implements OnInit {
 
+  @HostBinding('style') baseStyle: SafeStyle;
+  
   @Input()
   public options!: ITileConfig;
 
+  constructor(){
+    this.options = {
+      targetURL: '',
+      imageURL: '',
+      background: '',
+      text: '',
+      row: 0,
+      column: 0,
+      width: 0,
+      height: 0,
+      isVisible: false,
+    };
+
+    this.baseStyle = this.getStyles();
+  }
+
   ngOnInit(): void {
+    this.baseStyle = this.getStyles();
+  }
+
+  public getStyles() {
+    return ( this.options ?
+    {
+      'cursor': this.options.isActive ? 'pointer' : 'default',
+      'background-image': this.options.imageURL ? `url(${`assets/images/${this.options.imageURL}`})` : '',
+      'background-color': this.options.background === '' ? 'rgb(88,68,86)' : this.options.background,
+      'visibility': (this.options.isVisible ? 'visible' : 'hidden'),
+      'grid-column-start': this.options.column,
+      'grid-column-end': `span ${this.options.width}`,
+      'grid-row-start': this.options.row,
+      'grid-row-end': (this.options.row + this.options.height)
+    } : 
+    {});
   }
 }
--- src/app/modules/shared/tile-menu/components/tile-menu/tile-menu.component.ts
+++ src/app/modules/shared/tile-menu/components/tile-menu/tile-menu.component.ts
@@ -10,19 +10,4 @@ import { ITileConfig } from '../../interfaces';
 export class TileMenuComponent {
   @Input()
   public tilesOptions!: ITileConfig[];
-
-  public getStyles(options: ITileConfig) {
-    return ( options ?
-    {
-      'cursor': options.isActive ? 'pointer' : 'default',
-      'background-image': options.imageURL ? `url(${`assets/images/${options.imageURL}`})` : '',
-      'background-color': options.background === '' ? 'rgb(88,68,86)' : options.background,
-      'visibility': (options.isVisible ? 'visible' : 'hidden'),
-      'grid-column-start': options.column,
-      'grid-column-end': `span ${options.width}`,
-      'grid-row-start': options.row,
-      'grid-row-end': (options.row + options.height)
-    } : 
-    {});
-  }
 }
