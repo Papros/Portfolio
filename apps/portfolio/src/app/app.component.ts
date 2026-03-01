f6578117e8148071945c3c0d17bb67666b5dd5e9
Papros <piotrpapros.it@gmail.com>
2026-03-01 19:17:05 +0100

    [FIX] Fixing versioning

--- apps/portfolio/src/app/app.component.ts
+++ apps/portfolio/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { BUILD_VERSION } from '../environments/version';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,11 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    console.log(`App initialized. Build version: ${BUILD_VERSION}`);
+  }
+
   constructor() {
     const params = new URLSearchParams(window.location.search);
     const redirect = params.get('redirect');
