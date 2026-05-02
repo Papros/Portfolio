1b3fecdcfeafe022755888e156716b90b0ece50e
Papros <piotrpapros.it@gmail.com>
2026-05-03 00:49:33 +0200

    debug

--- libs/portfolio/modules/tutorial-module/src/lib/pages/tutorial-menu-page/tutorial-menu-page.component.ts
+++ libs/portfolio/modules/tutorial-module/src/lib/pages/tutorial-menu-page/tutorial-menu-page.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 interface TutorialCard {
   title: string;
   description: string;
+  image: string;
   route: string;
   tags: string[];
   available: boolean;
@@ -12,27 +14,67 @@ interface TutorialCard {
 
 @Component({
   selector: 'lib-tutorial-menu-page',
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, MatIconModule],
   templateUrl: './tutorial-menu-page.component.html',
   styleUrl: './tutorial-menu-page.component.scss',
 })
 export class TutorialMenuPageComponent {
+  placeholder = 'assets/images/component-demo/component-placeholder.png';
+
   readonly tutorials: TutorialCard[] = [
     {
       title: 'RxJS Playground',
       description:
         'Marble diagrams, operatory pipe, kombinowanie streamów. Nauka przez interaktywne wyzwania.',
+      image: 'assets/images/tutorials/rxjs-icon.png',
       route: 'rxjs',
       tags: ['rxjs', 'observables', 'operators'],
       available: true,
     },
     {
-      title: 'Change Detection',
+      title: 'RxJS Playground',
       description:
-        'Zrozum jak Angular decyduje kiedy re-renderować komponenty. OnPush, signals, zones.',
-      route: 'change-detection',
-      tags: ['angular', 'performance', 'signals'],
-      available: false,
+        'Marble diagrams, operatory pipe, kombinowanie streamów. Nauka przez interaktywne wyzwania.',
+      image: '',
+      route: 'rxjs',
+      tags: ['rxjs', 'observables', 'operators'],
+      available: true,
+    },
+    {
+      title: 'RxJS Playground',
+      description:
+        'Marble diagrams, operatory pipe, kombinowanie streamów. Nauka przez interaktywne wyzwania.',
+      image: '',
+      route: 'rxjs',
+      tags: ['rxjs', 'observables', 'operators'],
+      available: true,
+    },
+    {
+      title: 'RxJS Playground',
+      description:
+        'Marble diagrams, operatory pipe, kombinowanie streamów. Nauka przez interaktywne wyzwania.',
+      image: 'assets/images/tutorials/rxjs-icon.png',
+      route: 'rxjs',
+      tags: ['rxjs', 'observables', 'operators'],
+      available: true,
+    },
+    {
+      title: 'RxJS Playground',
+      description:
+        'Marble diagrams, operatory pipe, kombinowanie streamów. Nauka przez interaktywne wyzwania.',
+      image: 'assets/images/tutorials/rxjs-icon.png',
+      route: 'rxjs',
+      tags: ['rxjs', 'observables', 'operators'],
+      available: true,
+    },
+    {
+      title: 'RxJS Playground',
+      description:
+        'Marble diagrams, operatory pipe, kombinowanie streamów. Nauka przez interaktywne wyzwania.',
+      image: 'assets/images/tutorials/rxjs-icon.png',
+      route: 'rxjs',
+      tags: ['rxjs', 'observables', 'operators'],
+      available: true,
     },
   ];
 }
